c8ffa91430520fd1007dcb63c921c587d3733032
grysha11 <grzhzahar@gmail.com>
2026-06-10 13:02:47 +0200

    add hashing and check for passwords, add password hash in db

---
+++ backend/internal/auth/auth.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"unicode"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
+
+func CheckHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err
+}
+
+func IsValidPassword(password string) error {
+	var (
+		hasMinLen = false
+		hasUpper = false
+		hasLower = false
+		hasNumber = false
+		hasSpecial = false
+	)
+
+	if len(password) >= 8 {
+		hasMinLen = true
+	}
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	if hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial {
+		return nil
+	}
+
+	return fmt.Errorf("Not valid password")
+}
--- backend/sql/schema/001_users.sql
+++ backend/sql/schema/001_users.sql
@@ -2,7 +2,8 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT UNIQUE NOT NULL,
-    EMAIL TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    hashed_password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
