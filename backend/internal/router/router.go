a342d139c3f40290e528f69872906251994d86ad
grysha11 <grzhzahar@gmail.com>
2026-06-18 13:15:34 +0200

    add swagger documentation

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -9,8 +9,21 @@ import (
 )
 
 func NewRouter(cfg *config.Config, h *handler.Handler) *http.ServeMux {
-	strictHandler := api.NewStrictHandler(h, nil)
 	mux := http.NewServeMux()
+
+	mux.HandleFunc(http.MethodGet+" /api/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./api/openapi.yaml")
+	})
+
+	mux.HandleFunc(http.MethodGet+" /docs", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./api/docs.html")
+	})
+
+	mux.HandleFunc(http.MethodGet+" /swagger", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/docs", http.StatusMovedPermanently)
+	})
+	
+	strictHandler := api.NewStrictHandler(h, nil)
 	api.HandlerFromMux(strictHandler, mux)
 
 	return mux
